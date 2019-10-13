2f8c8542739138ad46659bd1afffb17db1dc99bd
vishnu raveendran <vishnuraveendran246@gmail.com>
2019-10-13 21:41:43 +0400

    Add existing file

---
+++ Assets/Scripts/SplashScreen.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SplashScreen : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Image splashimage;
+    public string loadlevel;
+
+    IEnumerator Start()
+    {
+        splashimage.canvasRenderer.SetAlpha(0.0f);
+        FadeIn();
+        yield return new WaitForSeconds(2.5f);
+        FadeOut();
+        yield return new WaitForSeconds(2.5f);
+        SceneManager.LoadScene(loadlevel);
+    }
+
+    // Update is called once per frame
+    void FadeIn()
+    {
+        splashimage.CrossFadeAlpha(1.0f, 1.5f, false);
+    }
+
+    void FadeOut()
+    {
+        splashimage.CrossFadeAlpha(0.0f, 2.5f, false);
+    }
+}
