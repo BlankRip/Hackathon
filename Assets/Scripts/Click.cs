550e8ac6f3b5a3e9c82b8892efa2797e2fe424ba
Aniketh <a.gondesi@student.saedubai.com>
2019-10-13 15:03:53 +0400

    Game scene started

---
+++ Assets/Scripts/Click.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Click : MonoBehaviour
+{
+    [SerializeField] Transform coin; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 initialScale = coin.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClick()
+    {
+        coin.localScale = new Vector3();
+    }
+}
